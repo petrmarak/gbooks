d10ea7d66a04028d30bf49958b35a8c279a34592
Petr Marak <p_marak@utb.cz>
2024-01-21 19:45:37 +0100

    Implemented basic searching for books via Google Books API.

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  foundBooksArray: any[] = [];
+  bookSearchString: String = "";
 
-  constructor() {}
+  constructor(public httpClient: HttpClient) {}
+
+  performSearch(bookSearchString: String) {
+    this.foundBooksArray = [];
+
+    const url = `${environment.baseApiUrl}?q=${bookSearchString}&key=${environment.apiKey}`;  // potenciálně doplnit inauthor
+    this.httpClient.get(url).subscribe(data => {
+      console.log(data);
+      this.foundBooksArray.push(data);
+    });
+
+  }
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  baseApiUrl: "https://www.googleapis.com/books/v1/volumes",
+  apiKey: "AIzaSyA41OBkWZc9gO3Lj2gLNIi13Zy5A90ExxQ"
 };
 
 /*
