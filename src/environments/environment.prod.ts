afe0c728088150dca96b054b09d2128e185e27cc
Petr Marak <p_marak@utb.cz>
2024-01-29 06:22:23 +0100

    • Generated PWA using @angular/pwa. • Custom app name and icons. • Set links that refer to websites outside this app to open in a new tab / window.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   baseUrl: "https://www.googleapis.com/books/v1/volumes",
-  apiKey: "AIzaSyA41OBkWZc9gO3Lj2gLNIi13Zy5A90ExxQ"
+  apiKey: ""
 };
+
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   baseUrl: "https://www.googleapis.com/books/v1/volumes",
-  apiKey: "AIzaSyA41OBkWZc9gO3Lj2gLNIi13Zy5A90ExxQ"
+  apiKey: ""
 };
 
 /*
@@ -16,3 +16,4 @@ export const environment = {
  * on performance if an error is thrown.
  */
 // import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+
